8397fe1de8fbd928b6f03c931d82f611c83481d1
test <test>
2023-11-26 23:41:16 +0900

    laravel一式作成

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    //index
+    public function index(Request $request) {
+        $coolString = 'Hello from Controller.';
+        return view('hello', compact('coolString'));
+    }
+}
